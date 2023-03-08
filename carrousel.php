e398c00b9b8635a04b79bafecda80c6b5fd1e328
Bargid <120350488+Bargid@users.noreply.github.com>
2023-03-08 10:51:26 -0500

    C11 -- Début du carrousel.

---
+++ carrousel.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Extension carrousel qui permet d'afficher dans une boite modale, les images d'une galerie
+ * Package name : Carrousel
+ * Version: 1.0.0
+ */
+
+
+ /**
+  * Plugin name: Carrousel
+  * Author: Louis Roby
+  * Plugin URI: https:github.com/eddytuto/carrousel 
+  * Description: Permet d'afficher dans une boite modale, les images d'une galerie avec un système de navigation
+  */
+?>
